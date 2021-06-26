606b302ec022d93d3a916568775a4ef85ecb2ede
deniskodak <denis.kodak@gmail.com>
2021-06-26 22:50:21 +0300

    make Cast component

--- src/Components/Cast/Cast.js
+++ src/Components/Cast/Cast.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Cast = ({ acters }) => {
-  return <h1>hello</h1>;
+const Cast = ({ acters: { cast } }) => {
+  console.log(cast);
+  return (
+    <ul>
+      {cast.map(({ id, character, original_name, profile_path }) => {
+        return (
+          <li key={id}>
+            <img
+              src={`http://image.tmdb.org/t/p/w185/${profile_path}`}
+              alt=""
+            />
+            <p>{original_name}</p>
+            <p>Character: {character}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Cast;
