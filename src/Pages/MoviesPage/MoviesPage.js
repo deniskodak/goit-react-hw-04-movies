c1424aa65c339ead6a09028fa4803c502336204a
deniskodak <denis.kodak@gmail.com>
2021-07-04 17:20:19 +0300

    add title in MoviesPage

--- src/Pages/MoviesPage/MoviesPage.js
+++ src/Pages/MoviesPage/MoviesPage.js
@@ -9,6 +9,7 @@ const apiService = new ApiService();
 
 class MoviesPage extends Component {
   state = {
+    keyword: "",
     movies: [],
     page: 1,
   };
@@ -17,26 +18,42 @@ class MoviesPage extends Component {
     const { location } = this.props;
 
     if (location.search) {
-      this.getMovies(location.search);
+      this.setState({ keyword: location.search.substring(1) });
+    }
+  }
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevState.keyword !== this.state.keyword) {
+      this.getMovies();
     }
   }
 
-  getMovies = async (keyword) => {
+  getMovies = async () => {
+    const { keyword } = this.state;
+    const movies = await apiService.getMoviesByQuery(keyword);
+    this.setState({ movies }, this.saveQueryInLocation());
+  };
+  getKeyword = async (keyword) => {
+    this.setState({ keyword });
+  };
+
+  saveQueryInLocation = () => {
+    const { keyword } = this.state;
     const { location } = this.props;
     location.search = keyword;
-
-    const movies = await apiService.getMoviesByQuery(keyword);
-    this.setState({ movies });
   };
 
   render() {
-    const { movies } = this.state;
+    const { movies, keyword } = this.state;
 
     return (
       <section className={styles.section}>
-        <Form onSubmit={this.getMovies} />
-
-        {movies && movies.length > 0 && <MoviesList movies={movies} />}
+        <Form onSubmit={this.getKeyword} />
+        {keyword && (
+          <h2 className={styles.title}>
+            Вот что мы нашли по запросу: {keyword}{" "}
+          </h2>
+        )}
+        {movies.length > 0 && <MoviesList movies={movies} />}
       </section>
     );
   }
